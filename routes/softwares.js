99a87dbf0b36e30201d9b70425064092e2ccd960
Eduardo <eduardoarevaloportero@gmail.com>
2025-02-23 17:48:19 +0100

    falta un parentesis uwu

--- routes/softwares.js
+++ routes/softwares.js
@@ -58,7 +58,7 @@ router.post('/edit/:id', isAuthenticated, async (req, res) => {
         const updatedSoftware = await Software.findByIdAndUpdate(
             id,
             { description, link },
-            { new: true }
+            { new: true })
         if (!updatedSoftware) {
             return res.status(404).send("Software no encontrado");
         }
