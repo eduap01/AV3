0ec4c2ac845507caca73397a256d20902a181c82
Salsamante <dejota98@gmail.com>
2025-02-25 20:33:41 +0100

    se pueden subir archivos ft ivan

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(cookieParser());
 
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use("/public", express.static(path.resolve(__dirname + '/public')));
+app.use("/files", express.static(path.join(__dirname, "files")));
 app.use(session({
   secret: 'mysecretsession',
   resave: false,
